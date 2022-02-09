d78f24e78adc5178251dd1cebc42ed13c0778cbb
Nikita Lezhnev <catassasin331@gmail.com>
2022-02-09 10:55:54 +0300

    Add ConferenceController.php

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(): Response
+    {
+        return new Response(
+            <<<EOF
+<html>
+    <body>
+        <img src="/images/under_construction.gif" />
+    </body>
+</html>
+EOF
+        );
+    }
+}
